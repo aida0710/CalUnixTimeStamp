c9c78d55c1a8ddb612a06a5db1f5fbb98945ef10
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-07-29 09:39:13 +0900

    計算部分を作成

--- Cal.php
+++ Cal.php
@@ -4,6 +4,10 @@ declare(strict_types = 1);
 
 class Cal {
 
+    /**
+     * @param Time $time
+     * @return int(unix timestamp)
+     */
     public function execute(Time $time): int {
         $year = $time->getYear();
         $month = $time->getMonth();
@@ -17,7 +21,16 @@ class Cal {
             $month += 12;
         }
 
+        $kEpocDays = 719163;
+        $kSecondsInDays = 86400;
 
+        $countBy4 = $year / 4;
+        $countBy400 = $year / 400;
+        $countBy100 = $year / 100;
+
+        $shiftedDay = (306 * ($month + 1)) / 10;
+        $dayCount = (365 * $year + $countBy4 - $countBy100 + $countBy400 + $shiftedDay + $day - 428) - $kEpocDays;
+        return $dayCount * $kSecondsInDays + ($hour * 3600) + ($minute * 60) + $second;
     }
 
 }
